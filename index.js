b3869cf21b9b237fb9b8d3c6503f42faa3855294
Sajal <paudyalsajal@gmail.com>
2020-09-02 16:36:00 +0545

    calc commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+		function valueButton(e){
+			calculatorForm.screen.value += e.value;
+		}
+		function Clear(){
+			calculatorForm.screen.value = null;
+		}
+		function Calculate(){
+			calculatorForm.screen.value = eval(calculatorForm.screen.value);
+		}
